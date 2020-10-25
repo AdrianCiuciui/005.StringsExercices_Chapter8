a2675daf06f3427be75ba496a21540ee4b2fbad6
AdrianCiuciui <adrian.ciuciui.cornel@gmail.com>
2020-10-25 13:13:39 +0200

    Ex5 started

---
+++ src/com/company/Exercise5.java
@@ -0,0 +1,15 @@
+package com.company;
+
+public class Exercise5 {
+
+//    The Captain Crunch decoder ring works by taking each letter in a string and adding 13 to it.
+//    For example, ’a’ becomes ’n’ and ’b’ becomes ’o’. The letters “wrap around” at the end, so ’z’ becomes ’m’.
+//    Write a method that takes a String and that returns a new String containing the encoded version.
+//    You should assume that the String contains upper and lower case letters, and spaces, but no other punctuation.
+//        Lower case letters should be tranformed into other lower case letters; upper into upper.
+//    You should not encode the spaces.
+
+    private String string;
+
+
+}
